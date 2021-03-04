e9bcc324e43e45037af135e0dacf43e0d22bb179
Jessica Seo <jessicaseo83@gmail.com>
2021-03-04 16:53:41 -0500

    Implement BergerIngredient component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,39 @@
+import def from 'ajv/dist/vocabularies/applicator/additionalItems';
+import React from 'react';
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className="BreadBottom"></div>;
+      break;
+    case ('bread-top'):
+      ingredient = (
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredient = <div className="Meat"></div>
+      break;
+    case ('cheese'):
+      ingredient = <div className="Cheese"></div>
+      break;
+    case ('bacon'):
+      ingredient = <div className="Bacon"></div>
+      break;
+    case ('veggie'):
+      ingredient = <div className="Veggie"></div>
+      break;
+    default:
+      ingredient: null;
+    
+    return ingredient;
+
+  }
+}
+
+export default burgerIngredient;

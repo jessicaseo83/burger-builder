7a0fd5e230db6c9a86110c774f0539d08aa56794
Jessica Seo <jessicaseo83@gmail.com>
2021-03-22 13:30:04 -0400

    Add ingredientAdded prop

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
 import './BuildControls.css';
 
 export default function BuildControls(props) {
+  const controls = [
+    { label: 'Veggie', type: 'veggie' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+
+  ];
   return (
     <div className="BuildControls">
-      
+      {controls.map(control => (
+        <BuildControl 
+          key={control.label} 
+          label={control.label}
+          added={() => props.ingredientAdded(control.type)}
+        />
+      ))}
     </div>
   )
 }

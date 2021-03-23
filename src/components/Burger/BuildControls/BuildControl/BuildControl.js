76cf3eda489413b668da015083330fce1a029791
Jessica Seo <jessicaseo83@gmail.com>
2021-03-23 00:16:26 -0400

    Add disabled button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ export default function BuildControl(props) {
   return (
   <div className="BuildControl">
     <div className="Label">{props.label}</div>
-    <button className="Less">Less</button>
+    <button disabled={props.disabled} className="Less" onClick={props.removed}>Less</button>
     <button className="More" onClick={props.added}>More</button>
   </div>
   )

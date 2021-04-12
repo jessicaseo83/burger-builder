d407c5ff9e33872f6b3ef228e16ed0e04e49ff16
Jessica Seo <jessicaseo83@gmail.com>
2021-04-12 12:31:47 -0400

    Update OrderSummary props

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 export default function OrderSummary(props) {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -15,6 +16,9 @@ export default function OrderSummary(props) {
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+
     </Aux>
   )
 }
